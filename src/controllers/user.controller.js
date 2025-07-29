5d47752b04454d56f26d7672cbeaf503ca3a3447
appdep <appdeployment64@gmail.com>
2025-07-29 11:58:05 +0500

    improve error handling in verify email

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -60,7 +60,7 @@ export const verifyEmail = asyncHandler(async(req,res)=>{
   const user = await User.findOne({email})
 
   if(user){
-    throw new ApiError(409, "user already exist")
+    throw new ApiError(409, "user with this email already exist")
   }
 
 
@@ -89,29 +89,29 @@ export const verifyEmail = asyncHandler(async(req,res)=>{
       purpose: 'registration'
     });
 
-    // Send email using nodemailer
-    try {
-      await transporter.sendMail({
-        from: process.env.EMAIL_FROM,
-        to: email,
-        subject: "Email Verification OTP",
-        text: `Your Email Verification OTP is ${otp}. It will expire in 20 minutes.`,
-        html: `
-          <div>
-            <h3>Email Verification Request</h3>
-            <p>Your OTP code is: <strong>${otp}</strong></p>
-            <p>This code will expire in 5 minutes.</p>
-          </div>
-        `
-      });
-    } catch (error) {
-     res.json(500,"Failed to  Verifiy email");
-      // Clean up OTP if email fails
-      await Otp.deleteOne({ email, purpose: 'registration' });
-      return res.json(
-         500 ,"Failed to send reset email"
-      );
-    }
+    // // Send email using nodemailer
+    // try {
+    //   await transporter.sendMail({
+    //     from: process.env.EMAIL_FROM,
+    //     to: email,
+    //     subject: "Email Verification OTP",
+    //     text: `Your Email Verification OTP is ${otp}. It will expire in 20 minutes.`,
+    //     html: `
+    //       <div>
+    //         <h3>Email Verification Request</h3>
+    //         <p>Your OTP code is: <strong>${otp}</strong></p>
+    //         <p>This code will expire in 5 minutes.</p>
+    //       </div>
+    //     `
+    //   });
+    // } catch (error) {
+    //  res.json(500,"Failed to  Verifiy email");
+    //   // Clean up OTP if email fails
+    //   await Otp.deleteOne({ email, purpose: 'registration' });
+    //   return res.json(
+    //      500 ,"Failed to send reset email"
+    //   );
+    // }
 
 
     return res.status(200).json(
