8c07fc1998684a872445dfc4be48416e57ea7acf
Hafiz Muhammad Abdullah <hm.abdullah@2bvision.com>
2025-09-05 00:09:09 +0500

    add ffmeg and posturl and other enhance thumbnail for every video and store and product info enhance

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -17,45 +17,37 @@ const router = express.Router();
 // Get all posts
 router.get("/getall", VerifyJwt, getAllPosts);
 
-// // Create/publish a new post
-// router.post("/create",
-//     VerifyJwt,
-//     upload.fields([
-//         { name: "imageFiles", maxCount: 10 },  // Multiple images
-//         { name: "videoFiles", maxCount: 5 },   // Multiple videos
-//         { name: "audioFiles", maxCount: 5 }, 
-//          { name: "song", maxCount: 6 }  ,  // Multiple audio files
-//         { name: "thumbnail", maxCount: 1 }     // Single thumbnail
-//     ]),
-//     publishPost
-// );
-
-// new for  imagefile1
-// Create/publish a new post
+// Create/publish a new post with numbered fields and thumbnails
 router.post("/create",
     VerifyJwt,
     upload.fields([
+        // Image files (numbered 1-5)
         { name: "imageFile1", maxCount: 1 }, 
-         { name: "imageFile2", maxCount: 1 }, 
-         { name: "imageFile3", maxCount: 1 }, 
-         { name: "imageFile4", maxCount: 1 }, 
-         { name: "imageFile5", maxCount: 1 }, 
-         // Multiple images
-        { name: "videoFile1", maxCount: 1 },   // Multiple videos
+        { name: "imageFile2", maxCount: 1 }, 
+        { name: "imageFile3", maxCount: 1 }, 
+        { name: "imageFile4", maxCount: 1 }, 
+        { name: "imageFile5", maxCount: 1 }, 
+        
+        // Video files (numbered 1-5)
+        { name: "videoFile1", maxCount: 1 },
         { name: "videoFile2", maxCount: 1 }, 
         { name: "videoFile3", maxCount: 1 }, 
         { name: "videoFile4", maxCount: 1 }, 
         { name: "videoFile5", maxCount: 1 }, 
         
+        // Thumbnails for each video (numbered 1-5)
+        { name: "thumbnail1", maxCount: 1 },
+        { name: "thumbnail2", maxCount: 1 },
+        { name: "thumbnail3", maxCount: 1 },
+        { name: "thumbnail4", maxCount: 1 },
+        { name: "thumbnail5", maxCount: 1 },
+        
+        // Audio files (keeping original structure)
         { name: "audioFiles", maxCount: 5 }, 
-         { name: "song", maxCount: 6 }  ,  // Multiple audio files
-        { name: "thumbnail", maxCount: 1 }     // Single thumbnail
+        { name: "song", maxCount: 6 }
     ]),
     publishPost
 );
-
-
-
 
 // Get a specific post by ID
 router.get("/:postId", VerifyJwt, getPostById);
@@ -63,14 +55,34 @@ router.get("/:postId", VerifyJwt, getPostById);
 // Delete a post
 router.delete("/:postId", VerifyJwt, deletePost);
 
-// Update a post
+// Update a post - also needs to be updated with new structure
 router.patch("/:postId",
     VerifyJwt,
     upload.fields([
-        { name: "imageFiles", maxCount: 10 },  // Additional images
-        { name: "videoFiles", maxCount: 5 },   // Additional videos
-        { name: "audioFiles", maxCount: 5 },   // Additional audio files
-        { name: "thumbnail", maxCount: 1 }     // Update thumbnail
+        // Image files (numbered 1-5)
+        { name: "imageFile1", maxCount: 1 }, 
+        { name: "imageFile2", maxCount: 1 }, 
+        { name: "imageFile3", maxCount: 1 }, 
+        { name: "imageFile4", maxCount: 1 }, 
+        { name: "imageFile5", maxCount: 1 }, 
+        
+        // Video files (numbered 1-5)
+        { name: "videoFile1", maxCount: 1 },
+        { name: "videoFile2", maxCount: 1 }, 
+        { name: "videoFile3", maxCount: 1 }, 
+        { name: "videoFile4", maxCount: 1 }, 
+        { name: "videoFile5", maxCount: 1 }, 
+        
+        // Thumbnails for each video (numbered 1-5)
+        { name: "thumbnail1", maxCount: 1 },
+        { name: "thumbnail2", maxCount: 1 },
+        { name: "thumbnail3", maxCount: 1 },
+        { name: "thumbnail4", maxCount: 1 },
+        { name: "thumbnail5", maxCount: 1 },
+        
+        // Audio files (keeping original structure)
+        { name: "audioFiles", maxCount: 5 },
+        { name: "song", maxCount: 6 }
     ]),
     updatePost
 );
@@ -85,6 +97,110 @@ router.get("/:postId/social/:linkType", incrementSocialLinkView);
 router.patch("/:postId/remove-media", VerifyJwt, removeMediaFiles);
 
 export default router;
+
+
+
+
+
+
+
+// import express from 'express';
+// import {
+//     deletePost,
+//     getAllPosts,
+//     getPostById,
+//     publishPost,
+//     togglePublishStatus,
+//     updatePost,
+//     incrementSocialLinkView,
+//     removeMediaFiles
+// } from "../controllers/post.controller.js";
+// import VerifyJwt from "../middlewares/auth.middleware.js";
+// import { upload } from "../middlewares/multer.middleware.js";
+
+// const router = express.Router();
+
+// // Get all posts
+// router.get("/getall", VerifyJwt, getAllPosts);
+
+// // // Create/publish a new post
+// // router.post("/create",
+// //     VerifyJwt,
+// //     upload.fields([
+// //         { name: "imageFiles", maxCount: 10 },  // Multiple images
+// //         { name: "videoFiles", maxCount: 5 },   // Multiple videos
+// //         { name: "audioFiles", maxCount: 5 }, 
+// //          { name: "song", maxCount: 6 }  ,  // Multiple audio files
+// //         { name: "thumbnail", maxCount: 1 }     // Single thumbnail
+// //     ]),
+// //     publishPost
+// // );
+
+// // new for  imagefile1
+// // Create/publish a new post
+// router.post("/create",
+//     VerifyJwt,
+//     upload.fields([
+//         { name: "imageFile1", maxCount: 1 }, 
+//          { name: "imageFile2", maxCount: 1 }, 
+//          { name: "imageFile3", maxCount: 1 }, 
+//          { name: "imageFile4", maxCount: 1 }, 
+//          { name: "imageFile5", maxCount: 1 }, 
+//          // Multiple images
+//         { name: "videoFile1", maxCount: 1 },   // Multiple videos
+//         { name: "videoFile2", maxCount: 1 }, 
+//         { name: "videoFile3", maxCount: 1 }, 
+//         { name: "videoFile4", maxCount: 1 }, 
+//         { name: "videoFile5", maxCount: 1 }, 
+        
+//         { name: "audioFiles", maxCount: 5 }, 
+//          { name: "song", maxCount: 6 }  ,  // Multiple audio files
+//         { name: "thumbnail", maxCount: 1 }     // Single thumbnail
+//     ]),
+//     publishPost
+// );
+
+
+
+
+// // Get a specific post by ID
+// router.get("/:postId", VerifyJwt, getPostById);
+
+// // Delete a post
+// router.delete("/:postId", VerifyJwt, deletePost);
+
+// // Update a post
+// router.patch("/:postId",
+//     VerifyJwt,
+//     upload.fields([
+//         { name: "imageFiles", maxCount: 10 },  // Additional images
+//         { name: "videoFiles", maxCount: 5 },   // Additional videos
+//         { name: "audioFiles", maxCount: 5 },   // Additional audio files
+//         { name: "thumbnail", maxCount: 1 }     // Update thumbnail
+//     ]),
+//     updatePost
+// );
+
+// // Toggle publish status
+// router.patch("/toggle/publish/:postId", VerifyJwt, togglePublishStatus);
+
+// // Increment social link view count
+// router.get("/:postId/social/:linkType", incrementSocialLinkView);
+
+// // Remove specific media files from post
+// router.patch("/:postId/remove-media", VerifyJwt, removeMediaFiles);
+
+// export default router;
+
+
+
+
+
+
+
+
+
+
 
 // Alternative format (commented out) - using Router() with .use() middleware
 // import { Router } from 'express';
